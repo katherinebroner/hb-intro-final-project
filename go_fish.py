dcf0d8c6e3ebd7f11ca3a0c8346d2256cb807ca1
Katherine Broner <katherine.broner@gmail.com>
2015-12-19 22:44:48 -0800

    full working game

--- go_fish.py
+++ go_fish.py
@@ -1,18 +1,28 @@
 #Set up cards
 
 import random
+import time
 
 rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
 deck_of_cards = (rank * 4)
+
 random.shuffle(deck_of_cards)
+
 player_1_hand = []
+
 computer_hand = []
+
 player_1_score = []
+
 computer_score = []
+
 def remove_card(random_card):
     deck_of_cards.remove(random_card)
+
 def ask_for_card():
     return raw_input ("What card would you like to ask for? ")
+
 def asked_for_everything(stored_card):
     for i in computer_hand:
         if i not in stored_card:
@@ -39,27 +49,39 @@ def check_for_book(hand, score):
             while i in hand:
                 hand.remove(i)
                 score.append(i)
-            print "There was a book in " + i
+            print "There was a book: " + i
 
 #Player 1 turn
 
 def player_1_turn():
     check_for_book(player_1_hand, player_1_score)
-    print sorted(player_1_hand)
-    while True:    
+    time.sleep(1)  
+    while len(player_1_hand) > 0:
+        print sorted(player_1_hand)
         ask = ask_for_card()  
         if ask not in computer_hand:
             break
         while ask in computer_hand:
             player_1_hand.append(ask)
-            computer_hand.remove(ask) 
-        check_for_book(player_1_hand, player_1_score)   
-        print sorted(player_1_hand)
+            computer_hand.remove(ask)
+            time.sleep(1) 
+            check_for_book(player_1_hand, player_1_score)
+    time.sleep(1)
     print "Go Fish Player 1!"
-    random_card = random.choice(deck_of_cards)
-    player_1_hand.append(random_card)
-    remove_card(random_card)
-    check_for_book(player_1_hand, player_1_score) 
+    time.sleep(1)
+    if len(deck_of_cards) > 0:
+        random_card = random.choice(deck_of_cards)
+        player_1_hand.append(random_card)
+        remove_card(random_card)
+        check_for_book(player_1_hand, player_1_score) 
+    else:
+        print "Game over!"
+        if len(player_1_score) > len(computer_score):
+            print "Player 1 wins!"
+        elif len(player_1_score) < len(computer_score):
+            print "Computer wins!"
+        else:
+            print "It's a tie!"
     
         
 #player_1_turn()
@@ -68,24 +90,39 @@ def player_1_turn():
 
 def computer_turn():
     check_for_book(computer_hand, computer_score)
+    time.sleep(1)
     stored_card = []
     while not asked_for_everything(stored_card):
         computer_card = random.choice(computer_hand)
         while computer_card in stored_card:
             computer_card = random.choice(computer_hand)
         stored_card.append(computer_card)
-        print "The computer asked for a " + computer_card
+        print "The computer asked for: " + computer_card
         if computer_card not in player_1_hand:
             break
         while computer_card in player_1_hand:
             computer_hand.append(computer_card)
             player_1_hand.remove(computer_card)
         check_for_book(computer_hand, computer_score)
+        time.sleep(1)
+        while len(computer_hand) < 0:
+            break
+    time.sleep(1)
     print "Go Fish Computer!"
-    random_card = random.choice(deck_of_cards)
-    computer_hand.append(random_card)
-    remove_card(random_card)
-    check_for_book(computer_hand, computer_score)
+    time.sleep(1)
+    if len(deck_of_cards) > 0:
+        random_card = random.choice(deck_of_cards)
+        computer_hand.append(random_card)
+        remove_card(random_card)
+        check_for_book(computer_hand, computer_score)
+    else:
+        print "Game over!"
+        if len(player_1_score) > len(computer_score):
+            print "Player 1 wins!"
+        elif len(player_1_score) < len(computer_score):
+            print "Computer wins!"
+        else:
+            print "It's a tie!"
 
 #computer_turn()
 
@@ -93,21 +130,12 @@ def computer_turn():
 
 def play_game():
     print "Welcome to Go Fish!"
+    print "How to play: Ask the Computer for a card. If you get 4 of the same cards, that is a book. The player with the most number of books wins!"
     passing_out_cards()
-    while True:
-        if deck_of_cards > 0 and len(player_1_hand) > 0 and len(computer_hand) > 0:
-            player_1_turn()
-            computer_turn()
-        else:
-            print "Game over!"
-            break
-    if len(player_1_score) > len(computer_score):
-        print "Player 1 wins!"
-    elif len(player_1_score) < len(computer_score):
-        print "Computer wins!"
-    else:
-        print "It's a tie!"
- 
+    while deck_of_cards > 0 and len(player_1_hand) > 0 and len(computer_hand) > 0:
+        player_1_turn()
+        computer_turn()
+   
 play_game()
 
 
